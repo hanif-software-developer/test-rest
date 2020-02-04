1317bf10eab9cc489671a2b32ca5d04e5f21178e
hanif-software-developer <hanif.software.developer@gmail.com>
2020-02-05 06:52:33 +0700

    starter

---
+++ src/main/java/co/id/hanif/dev/onetable/crudonetable/CrudOneTableApplication.java
@@ -0,0 +1,13 @@
+package co.id.hanif.dev.onetable.crudonetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOneTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOneTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/hanif/dev/onetable/crudonetable/CrudOneTableApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.hanif.dev.onetable.crudonetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOneTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
